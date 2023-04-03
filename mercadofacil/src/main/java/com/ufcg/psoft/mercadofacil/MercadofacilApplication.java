3161e5d9a024cb8b361d7503955c023bcf7e8e4e
adenou.junior <adenou.junior@ccc.ufcg.edu.br>
2023-04-03 08:08:57 -0300

    Mudanças na versão Java do projeto Spring.

--- mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/MercadofacilApplication.java
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/MercadofacilApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoMercadoFacilApplication {
+public class MercadofacilApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoMercadoFacilApplication.class, args);
+		SpringApplication.run(MercadofacilApplication.class, args);
 	}
-
 
 }
